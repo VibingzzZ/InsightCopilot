066107d66758209d6e97431509beaa56a0ba45e9
Anonymous <2509094405@qq.com>
2026-09-24 15:38:05 +0800

    fix: 增加检测.env配置信息报错机制 - 删除多余的模型配置

--- app/integrations/model/gateway.py
+++ app/integrations/model/gateway.py
@@ -2,6 +2,9 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from openai import api_key
+
+from app.integrations import model
 
 load_dotenv()
 
@@ -14,13 +17,19 @@ class ModelGateway:
             model=os.getenv("MODEL_FAST"),
             api_key=os.getenv("DASHSCOPE_API_KEY"),
             base_url=os.getenv("DASHSCOPE_BASE_URL"),
+            temperature=0.5,
         )
         # 调用ds模型
         self.ds = ChatOpenAI(
             model=os.getenv("MODEL_DS"),
             api_key=os.getenv("DEEPSEEK_API_KEY"),
             base_url=os.getenv("DEEPSEEK_BASE_URL"),
+            temperature=0.5
         )
+        if not model:
+            raise ValueError("未设置模型，请检查.env文件配置")
+        if not api_key:
+            raise ValueError("未在环境变量中检测到 API_KEY，请检查 .env 文件配置")
 
     def get_fast_model(self):
         return self.llm
